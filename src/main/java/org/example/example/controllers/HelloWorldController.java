9b6e94c632f78a9444b833de1a487e48593aaa56
tym1226 <ymtao@hotmail.com>
2023-11-06 15:27:39 -0800

    added helloperson

--- src/main/java/org/example/example/controllers/HelloWorldController.java
+++ src/main/java/org/example/example/controllers/HelloWorldController.java
@@ -1,6 +1,8 @@
 package org.example.example.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +11,10 @@ public class HelloWorldController {
     @GetMapping("/")
     public String helloWorld() {
         return "hello world";
+    }
+
+    @GetMapping("/{name}")
+    public String helloPerson(@PathVariable("name") String name) {
+        return "Hello " + name + "!";
     }
 }
